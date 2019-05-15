6331851043731afe16e6eb40bb649f8e49913935
Daria <dariaurbanik@gmail.com>
2019-05-15 12:56:57 +0200

    Navbar

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
   }
